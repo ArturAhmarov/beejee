7b769ced440389faa496cbcee0d94b4cb19eaf8c
Artur <arturperm2000@gmail.com>
2022-11-30 07:23:00 +0500

    config

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -28,21 +28,6 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
-        'twbs/bootstrap' => array(
-            'pretty_version' => 'v5.0.2',
-            'version' => '5.0.2.0',
-            'reference' => '688bce4fa695cc360a0d084e34f029b0c192b223',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../twbs/bootstrap',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'twitter/bootstrap' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => 'v5.0.2',
-            ),
-        ),
         'yidas/pagination' => array(
             'pretty_version' => '1.0.7',
             'version' => '1.0.7.0',
